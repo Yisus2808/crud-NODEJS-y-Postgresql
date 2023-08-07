6cc5cb5e6ace1eb5919df5b9480ed31b085ed3f3
J. Jesús Espinoza P <espinozajuan147@gmail.com>
2023-08-07 11:49:35 -0700

    actualizado
    terminado

--- database.js
+++ database.js
@@ -27,57 +27,160 @@ pool.query('SELECT NOW()', (error, result) => {
     }
 });
 
-// Obtener los datos de empleados
-function obtenerDatosEmpleados(callback) {
-    pool.query('SELECT * FROM empleado', (error, results) => {
-        if (error) {
-            callback(error, null);
-        } else {
-            callback(null, results.rows);
-        }
-    });
-}
+/*########################
+CONSULTAS DE EMPLEADOS
+###############################*/
 
-// Obtener los datos de departamentos
-function obtenerDatosDepartamentos(callback) {
-    pool.query('SELECT * FROM departamento ORDER BY nombre ASC', (error, results) => {
-        if (error) {
-            callback(error, null);
-        } else {
-            callback(null, results.rows);
-        }
-    });
+// Obtener los datos de empleados
+// Obtener los datos de empleados
+async function obtenerDatosEmpleados() {
+    try {
+        const client = await pool.connect();
+        const query = 'SELECT * FROM empleado';
+        const result = await client.query(query);
+        client.release();
+        return result.rows;
+    } catch (error) {
+        throw error;
+    }
 }
 
 // Insertar empleado en la base de datos
-function insertarEmpleado(empleado, callback) {
-    const { nombre, apellido, telefono, direccion, fecha_nacimiento, observaciones, sueldo, departamento } = empleado;
+async function insertarEmpleado(empleado) {
+    const { nombre, apellido, telefono, direccion, fecha_nacimiento, observaciones, sueldo, id_departamento } = empleado;
 
-    // Realiza la inserción en la base de datos
-    pool.query('INSERT INTO empleado (nombre, apellido, telefono, direccion, fecha_nacimiento, observaciones, sueldo, departamento) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [nombre, apellido, telefono, direccion, fecha_nacimiento, observaciones, sueldo, departamento], (err, result) => {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null);
-        }
-    });
+    try {
+        // Realiza la inserción en la base de datos
+        await pool.query(
+            'INSERT INTO empleado (nombre, apellido, telefono, direccion, fecha_nacimiento, observaciones, sueldo, id_departamento) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [nombre, apellido, telefono, direccion, fecha_nacimiento, observaciones, sueldo, id_departamento]
+        );
+    } catch (error) {
+        throw error;
+    }
 }
 
-// Insertar departamento en la base de datos
-function insertarDepartamento(nombre, callback) {
-    // Realiza la inserción en la base de datos
-    pool.query('INSERT INTO departamento (nombre) VALUES ($1)', [nombre], (err, result) => {
-        if (err) {
-            callback(err);
-        } else {
-            callback(null);
-        }
-    });
+
+
+// Obtener un empleado por su ID
+async function obtenerEmpleadoPorId(id) {
+    try {
+        const client = await pool.connect();
+        const query = 'SELECT * FROM empleado WHERE id_empleado = $1';
+        const values = [id];
+        const result = await client.query(query, values);
+        client.release();
+        return result.rows[0];
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Actualizar empleado en la base de datos
+async function actualizarEmpleado(id, datosEmpleado) {
+    const { nombre, apellido, telefono, direccion, fecha_nacimiento, observaciones, sueldo, id_departamento } = datosEmpleado;
+
+    try {
+        const client = await pool.connect();
+        const query =
+            'UPDATE empleado SET nombre = $1, apellido = $2, telefono = $3, direccion = $4, fecha_nacimiento = $5, observaciones = $6, sueldo = $7, id_departamento = $8 WHERE id_empleado = $9';
+        const values = [nombre, apellido, telefono, direccion, fecha_nacimiento, observaciones, sueldo, id_departamento, id];
+        await client.query(query, values);
+        client.release();
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+// Eliminar empleado de la base de datos
+async function eliminarEmpleado(id) {
+    try {
+        const client = await pool.connect();
+        const query = 'DELETE FROM empleado WHERE id_empleado = $1';
+        const values = [id];
+        await client.query(query, values);
+        client.release();
+    } catch (error) {
+        throw error;
+    }
+}
+
+/*########################
+CONSULTAS DE DEPARTAMENTOS
+###############################*/
+
+// Obtener los datos de departamentos
+async function obtenerDatosDepartamentos() {
+    try {
+        const client = await pool.connect();
+        const query = 'SELECT * FROM departamento ORDER BY nombre ASC';
+        const result = await client.query(query);
+        client.release();
+        return result.rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Insertar departamento
+async function insertarDepartamento(nombre) {
+    try {
+        // Realiza la inserción en la base de datos
+        await pool.query('INSERT INTO departamento (nombre) VALUES ($1)', [nombre]);
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Obtener un departamento por su ID
+async function obtenerDepartamentoPorId(id) {
+    try {
+        const client = await pool.connect();
+        const query = 'SELECT * FROM departamento WHERE id_departamento = $1';
+        const values = [id];
+        const result = await client.query(query, values);
+        client.release();
+        return result.rows[0];
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Actualizar departamento en la base de datos
+async function actualizarDepartamento(id, nombre) {
+    try {
+        const client = await pool.connect();
+        const query = 'UPDATE departamento SET nombre = $1 WHERE id_departamento = $2';
+        const values = [nombre, id];
+        await client.query(query, values);
+        client.release();
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Eliminar departamento de la base de datos
+async function eliminarDepartamento(id) {
+    try {
+        const client = await pool.connect();
+        const query = 'DELETE FROM departamento WHERE id_departamento = $1';
+        const values = [id];
+        await client.query(query, values);
+        client.release();
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = {
     obtenerDatosEmpleados,
     obtenerDatosDepartamentos,
     insertarEmpleado,
-    insertarDepartamento
+    insertarDepartamento,
+    obtenerEmpleadoPorId,
+    actualizarEmpleado,
+    eliminarEmpleado,
+    obtenerDepartamentoPorId,
+    actualizarDepartamento,
+    eliminarDepartamento
 };
--- index.js
+++ index.js
@@ -1,95 +1,255 @@
 const express = require('express');
 const path = require('path');
-const { obtenerDatosEmpleados, obtenerDatosDepartamentos, insertarEmpleado, insertarDepartamento } = require('./database');
+const {
+    pool,
+    obtenerDatosEmpleados,
+    obtenerDatosDepartamentos,
+    insertarEmpleado,
+    insertarDepartamento,
+    obtenerEmpleadoPorId,
+    actualizarEmpleado,
+    eliminarEmpleado,
+    obtenerDepartamentoPorId,
+    actualizarDepartamento,
+    eliminarDepartamento
+} = require('./database');
 
 const app = express();
 
-// Para tener bodyparser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+/*
+#######################################################
+########### RUTAS PARA SERVIR LOS DIRECTORIOS
+########### VIEWS, JS, CSS
+#######################################################
+*/
 // Configuración de las vistas
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Define las rutas
+// Configuración de los archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define la ruta principal y la ruta empleado
-app.get(['/', '/empleado'], (req, res) => {
-    obtenerDatosEmpleados((errorEmpleados, datos_empleado) => {
-        if (errorEmpleados) {
-            // Manejo del error
-            console.error(errorEmpleados);
-            res.status(500).send('Error en el servidor');
-        } else {
-            obtenerDatosDepartamentos((errorDepartamentos, datos_departamento) => {
-                if (errorDepartamentos) {
-                    // Manejo del error
-                    console.error(errorDepartamentos);
-                    res.status(500).send('Error en el servidor');
-                } else {
-                    // Verifica que haya resultados antes de renderizar la vista
-                    if (datos_empleado.length > 0 && datos_departamento.length > 0) {
-                        res.render('empleado', { datos_empleado, datos_departamento }); // Pasar los datos al renderizar la plantilla
-                    } else {
-                        res.status(404).send('No se encontraron datos');
-                    }
-                }
-            });
-        }
-    });
+// Ruta para servir el archivo script.js
+app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'script.js'));
 });
 
-// Insertar empleado en la base datos
-app.post('/empleado', (req, res) => {
-    const { nombre, apellido, telefono, direccion, fecha_nacimiento, observaciones, sueldo, departamento } = req.body;
-
-    insertarEmpleado({ nombre, apellido, telefono, direccion, fecha_nacimiento, observaciones, sueldo, departamento },
-        (err) => {
-            if (err) {
-                console.error('Error al insertar en la base de datos:', err);
-                res.status(500).send('Error al procesar el formulario: ' + err.message);
-            } else {
-                res.status(200).send('Formulario enviado correctamente');
-            }
-        }
-    );
+// Ruta para servir el archivo styles.css
+app.get('/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'styles.css'));
 });
 
-// Obtener los datos de departamento
-app.get('/departamento', (req, res) => {
-    obtenerDatosDepartamentos((error, datos_departamento) => {
-        if (error) {
-            // Manejo del error
-            console.error(error);
-            res.status(500).send('Error en el servidor');
-        } else {
-            // Verifica que haya resultados antes de renderizar la vista
-            if (datos_departamento.length > 0) {
-                res.render('departamento', { datos_departamento });
-            } else {
-                res.status(404).send('No se encontraron datos');
-            }
-        }
-    });
+/*
+#######################################################
+########### RUTAS DE LA APLICACIÓN
+#######################################################
+*/
+
+// Ruta principal y ruta empleado
+app.get(['/', '/empleado'], async(req, res, next) => {
+    try {
+        // Obtener los datos de los empleados y departamentos
+        const datosEmpleados = await obtenerDatosEmpleados();
+        const datosDepartamentos = await obtenerDatosDepartamentos();
+
+        // Aquí defines el valor de las variables isEditing y currentEditingRowId
+        const isEditing = false; // O cualquier otro valor que corresponda a tu lógica
+        const currentEditingRowId = null; // O cualquier otro valor que corresponda a tu lógica
+
+        // Renderizar la vista empleado con los datos obtenidos y las variables de edición
+        res.render('empleado', {
+            datos_empleado: datosEmpleados,
+            datos_departamento: datosDepartamentos,
+            isEditing: isEditing,
+            currentEditingRowId: currentEditingRowId
+        });
+    } catch (error) {
+        console.error('Error en el servidor:', error);
+        next(error); // Pasar el error al siguiente middleware de manejo de errores
+    }
 });
 
-// Insertar departamento en la base datos
-app.post('/departamento', (req, res) => {
+
+// Insertar empleado en la base de datos
+app.post('/empleado', async(req, res, next) => {
+    const { nombre, apellido, telefono, direccion, fecha_nacimiento, observaciones, sueldo, id_departamento } = req.body;
+
+    try {
+        // Validar datos de entrada
+        if (!nombre) {
+            return res.status(400).send('El nombre es obligatorio');
+        }
+
+        // Verificar que el valor del departamento no sea nulo
+        if (id_departamento === null || id_departamento === undefined) {
+            return res.status(400).send('Debe seleccionar un departamento');
+        }
+
+        // Convertir el valor del campo "id_departamento" a un número entero
+        const idDepartamento = parseInt(id_departamento, 10);
+
+        // Verificar si el valor es un número válido
+        if (isNaN(idDepartamento)) {
+            return res.status(400).send('El ID del departamento debe ser un número válido');
+        }
+
+        // Insertar empleado en la base de datos
+        await insertarEmpleado({ nombre, apellido, telefono, direccion, fecha_nacimiento, observaciones, sueldo, id_departamento: idDepartamento });
+        res.status(200).send('Empleado insertado correctamente');
+    } catch (error) {
+        console.error('Error al insertar en la base de datos:', error);
+        next(error); // Pasar el error al siguiente middleware de manejo de errores
+    }
+});
+
+// Actualizar empleado en la base de datos
+app.put('/empleado/:id', async(req, res) => {
+    const empleadoId = req.params.id;
+    const { nombre, apellido, telefono, direccion, fecha_nacimiento, observaciones, sueldo, id_departamento } = req.body;
+
+    try {
+        // Verificar si el empleado existe antes de actualizar
+        const empleadoExistente = await obtenerEmpleadoPorId(empleadoId);
+        if (!empleadoExistente) {
+            return res.status(404).send('Empleado no encontrado');
+        }
+
+        // Actualizar empleado en la base de datos
+        await actualizarEmpleado(empleadoId, {
+            nombre,
+            apellido,
+            telefono,
+            direccion,
+            fecha_nacimiento,
+            observaciones,
+            sueldo,
+            id_departamento,
+        });
+
+        // Obtener los datos actualizados del empleado
+        const empleadoActualizado = await obtenerEmpleadoPorId(empleadoId);
+
+        // Enviar los datos actualizados del empleado como respuesta en formato JSON
+        res.status(200).json(empleadoActualizado);
+    } catch (error) {
+        console.error('Error al actualizar empleado:', error);
+        next(error);
+    }
+});
+
+
+// Eliminar empleado de la base de datos
+app.delete('/empleado/:id', async(req, res) => {
+    const empleadoId = req.params.id;
+
+    try {
+        // Verificar si el empleado existe antes de eliminar
+        const empleadoExistente = await obtenerEmpleadoPorId(empleadoId);
+        if (!empleadoExistente) {
+            return res.status(404).send('Empleado no encontrado');
+        }
+
+        // Eliminar empleado de la base de datos
+        await eliminarEmpleado(empleadoId);
+
+        res.status(200).send('Empleado eliminado correctamente');
+    } catch (error) {
+        console.error('Error al eliminar empleado:', error);
+        next(error);
+    }
+});
+
+// Obtener los datos de los departamentos
+app.get('/departamento', async(req, res) => {
+    try {
+        // Obtener los datos de los departamentos
+        const datosDepartamentos = await obtenerDatosDepartamentos();
+
+        // Renderizar la vista departamento con los datos obtenidos
+        res.render('departamento', { datos_departamento: datosDepartamentos });
+    } catch (error) {
+        console.error('Error en el servidor:', error);
+        next(error);
+    }
+});
+
+// Insertar departamento en la base de datos
+app.post('/departamento', async(req, res) => {
     const { nombre } = req.body;
 
-    insertarDepartamento(nombre, (err) => {
-        if (err) {
-            console.error('Error al insertar en la base de datos:', err);
-            res.status(500).send('Error al procesar el formulario: ' + err.message);
-        } else {
-            res.status(200).send('Formulario enviado correctamente');
-        }
-    });
+    try {
+        // Insertar departamento en la base de datos
+        await insertarDepartamento(nombre);
+        res.status(200).send('Formulario enviado correctamente');
+    } catch (error) {
+        console.error('Error al insertar en la base de datos:', error);
+        next(error);
+    }
 });
 
-// Inicia el servidor
+// Actualizar departamento en la base de datos
+app.put('/departamento/:id', async(req, res) => {
+    const departamentoId = req.params.id;
+    const { nombre } = req.body;
+
+    try {
+        // Verificar si el departamento existe antes de actualizar
+        const departamentoExistente = await obtenerDepartamentoPorId(departamentoId);
+        if (!departamentoExistente) {
+            return res.status(404).send('Departamento no encontrado');
+        }
+
+        // Actualizar departamento en la base de datos
+        await actualizarDepartamento(departamentoId, nombre);
+        res.status(200).json({ message: 'Departamento actualizado correctamente' });
+    } catch (error) {
+        console.error('Error al actualizar departamento:', error);
+        next(error);
+    }
+});
+
+// Eliminar departamento de la base de datos
+app.delete('/departamento/:id', async(req, res) => {
+    const departamentoId = req.params.id;
+
+    try {
+        // Verificar si el departamento existe antes de eliminar
+        const departamentoExistente = await obtenerDepartamentoPorId(departamentoId);
+        if (!departamentoExistente) {
+            return res.status(404).send('Departamento no encontrado');
+        }
+
+        // Eliminar departamento de la base de datos
+        await eliminarDepartamento(departamentoId);
+
+        res.status(200).send('Departamento eliminado correctamente');
+    } catch (error) {
+        console.error('Error al eliminar departamento:', error);
+        next(error);
+    }
+});
+
+// Middleware de manejo de errores
+app.use((err, req, res, next) => {
+    console.error('Error no controlado:', err);
+
+    // Verificar si el error es un error de la base de datos
+    if (err.code === 'ECONNREFUSED' || err.code === 'ENOTFOUND' || err.code === 'ECONNRESET') {
+        console.error('Error en la conexión a la base de datos:', err.message);
+        res.status(500).send('Error en la conexión a la base de datos');
+    } else {
+        // Otro tipo de error que no sea de la base de datos
+        console.error('Error interno del servidor:', err);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+
+
+// Iniciar el servidor
 app.listen(3000, () => {
     console.log('Servidor iniciado en el puerto 3000');
 });
